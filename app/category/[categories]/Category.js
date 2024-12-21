60f08e3988dd2a87a26e90de5fd301745964ba83
Kerlos Magdy <kerlosssony@gmail.com>
2024-12-21 14:09:58 +0200

    new build

--- app/category/[categories]/Category.js
+++ app/category/[categories]/Category.js
@@ -1,5 +1,5 @@
 "use client";
-import CardSkeleton from "@/components/CardSkeleton";
+import CardSkeleton from "../../../components/CardSkeleton";
 import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";

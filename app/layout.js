a2c85a3432af66adb43e80613fa456b1e63dce56
Kerlos Magdy <kerlosssony@gmail.com>
2024-12-24 10:12:31 +0200

    Google Search Console

--- app/layout.js
+++ app/layout.js
@@ -8,7 +8,7 @@ import NavBar from "../components/Header";
 const inter = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
-  metadataBase: new URL("https://cross-walk.vercel.app"),
+  metadataBase: new URL("https://ke-codify.vercel.app/"),
   title: {
       default: "KeCodify",
       template:
@@ -22,7 +22,7 @@ export const metadata = {
       "Welcome to KeCodify Interview Questions, your go-to resource for thoughtfully crafted questions designed to spark engaging and insightful conversations. Whether you're preparing for a professional interview, hosting a podcast, or conducting a panel discussion, our curated collection of questions covers a wide range of topics in marketing, branding, and advertising. Discover the tools you need to inspire meaningful dialogue, gain valuable insights, and explore strategies that empower businesses to thrive in today's dynamic landscape.",
       type: "website",
       locale: "en_US",
-      url: "https://cross-walk.vercel.app/",
+      url: "https://ke-codify.vercel.app/",
       siteName: "KeCodify",
   },
   icons: {
@@ -34,6 +34,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <meta name="google-site-verification" content="ymYfjvSdKiU-l4BrhY98KgFhOGRcRpehbUUsIKvpaW8" />
+      </head>
       <body className={inter.className}>
         <NextTopLoader color="#000" height={4} />
                 <NavBar />
